111e1427bde8c6ffc1c1fe3745e9750298416398
Rekha0722 <rekhadeshmane7@gmail.com>
2020-07-30 12:47:48 +0530

    changed Wrapper class name

---
+++ src/main/java/com/generic/Generic.java
@@ -0,0 +1,9 @@
+package com.generic;
+
+public class Generic {
+
+	public void add()
+	{
+		System.out.println("HEllo");
+	}
+}
---
+++ src/main/java/com/generic/WarpperMethods.java
@@ -0,0 +1,9 @@
+package com.generic;
+
+import org.openqa.selenium.WebDriver;
+
+public class WarpperMethods {
+	
+	public WebDriver driver;
+
+}
